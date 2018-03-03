2f20c6fb64ecf5a2c3ef5f103211ae4eda11fa5b
刘阳 <place222@hotmail.com>
2018-03-03 18:37:08 +0800

    first

---
+++ Client/Program.cs
@@ -0,0 +1,57 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Net.Http;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var disco = DiscoveryClient.GetAsync("http://localhost:5000").Result;
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+            }
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+            var tokenResponse = tokenClient.RequestClientCredentialsAsync("api1").Result;
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+            Console.WriteLine($"Token:{tokenResponse.Json}");
+
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+            var response = client.GetAsync("http://localhost:9000/api/values").Result;
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+            }
+            response = client.GetAsync("http://localhost:9000/api/test").Result;
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+            }
+            response = client.GetAsync("http://localhost:9000/api/agg").Result;
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OcelotApiGateway/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace OcelotApiGateway
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment environment)
+        {
+            var builder = new Microsoft.Extensions.Configuration.ConfigurationBuilder();
+            builder.SetBasePath(environment.ContentRootPath)
+                   .AddJsonFile("appsettings.json", false, reloadOnChange: true)
+                   .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
+                   .AddJsonFile("configuration.json", optional: false, reloadOnChange: true)
+                   .AddEnvironmentVariables();
+
+
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var authenticationProviderKey = "Bearer";
+            services.AddAuthentication()
+                .AddIdentityServerAuthentication(authenticationProviderKey, o =>
+                {
+                    o.Authority = "http://localhost:5000";
+                    o.ApiName = "api1";
+                    o.SupportedTokens = IdentityServer4.AccessTokenValidation.SupportedTokens.Both;
+                    o.ApiSecret = "secret";
+                    o.RequireHttpsMetadata = false;
+                });
+            services.AddOcelot(Configuration);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public async void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            await app.UseOcelot();
+
+        }
+    }
+}
